1bc4e960d217aa1ad5d321bbfa7e98ae7a3daa5e
Marcus Jun <mjun@calstate.edu>
2018-03-04 19:38:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
   var music=[]
   for (let i=0;i<musicians.length;i++){
+    //use push() not unshift()
     music.push(musicians[i]+" plays "+instruments[i])
   }
   return music
