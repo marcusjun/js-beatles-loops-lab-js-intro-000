723051c5a13a52770787f12b4447fba342a98810
Marcus Jun <mjun@calstate.edu>
2018-03-04 19:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function johnLennonFacts(array){
   var n=array.length
   //var exclam="!!!"
   while(n>0){
-    //array[n]=array[n]+"!!!"
-    array[n]+="!!!"
+    array[n]=array[n]+"!!!"
+    //array[n]+="!!!"
     n--
   }
   return array
